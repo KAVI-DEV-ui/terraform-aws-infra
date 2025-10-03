9214276bd6f0d328170d154efe048c9780574f35
KAVI-DEV-ui <ravichand.done@gmail.com>
2025-10-03 22:30:57 +0530

    Add files via upload

---
+++ Terraform-pipelines-projects-main/modules/ec2/main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "main" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  tags = { Name = "dev-instance" }
+}
---
+++ Terraform-pipelines-projects-main/modules/subnet/variables.tf
@@ -0,0 +1,2 @@
+variable "vpc_id" { type = string }
+variable "subnet_cidr" { type = string }
